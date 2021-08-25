8fb31c8b8f331e931894679e6d670b39a4a22448
basic-app <dev@basic-app.com>
2021-08-25 23:50:04 +0300

    n/a

--- AuthService.php
+++ AuthService.php
@@ -6,33 +6,22 @@
  */
 namespace BasicApp\Auth;
 
+use CodeIgniter\Cookie\Cookie;
+
 class AuthService extends BasicAuthService
 {
 
-    protected $tokenCookie;
-
-    protected $token;
-
-    public function __construct(string $sessionKey = 'user')
-    {
-        parent::__construct($sessionKey);
-
-        $this->tokenCookie = new TokenCookie($this->sessionKey . '_token');
-
-        $this->token = new Token($this->sessionKey . '_token');
-    }
-
-    public function setId($id, bool $rememberMe = true, int $expire = 0)
+    public function setId($id, bool $rememberMe = true, int $expires = 0)
     {
         $this->unsetId();
 
         if (!$rememberMe)
         {
-            $token = $this->token->generate();
+            $token = $this->generateToken();
 
-            $this->token->set($token);
+            $this->setToken($token);
 
-            $this->tokenCookie->set($token, $expire);
+            $this->setTokenCookie($token, $expires);
         }
 
         return parent::setId($id);
@@ -44,11 +33,11 @@ class AuthService extends BasicAuthService
 
         if ($return)
         {
-            $token = $this->token->get();
+            $token = $this->getToken();
 
             if ($token)
             {         
-                if ($this->tokenCookie->get() != $token)
+                if ($this->getTokenCookie() != $token)
                 {
                     $this->unsetId();
 
@@ -66,11 +55,54 @@ class AuthService extends BasicAuthService
 
     public function unsetId()
     {
-        $this->tokenCookie->delete();
+        $this->removeTokenCookie();
 
-        $this->token->remove();
+        $this->removeToken();
 
         return parent::unsetId();
+    }
+
+    public function generateToken()
+    {
+        return md5(time() . rand(0, PHP_INT_MAX)); 
+    }
+
+    public function getToken()
+    {
+        return service('session')->get($this->sessionKey . '_token');
+    }
+
+    public function removeToken()
+    {
+        return service('session')->remove($this->sessionKey . '_token');
+    }
+
+    public function setToken(string $token)
+    {
+        return service('session')->set($this->sessionKey . '_token', $token);
+    }
+
+    public function getTokenCookie()
+    {
+        return service('request')->getCookie($this->sessionKey . '_token');
+    }
+
+    public function setTokenCookie(string $content, $expires = 0)
+    {
+        $cookie = new Cookie($this->sessionKey . '_token', $content, [
+            'expires' => $expires,
+            'httponly' => false
+        ]);
+
+        cookies()->put($cookie)->dispatch();
+    }
+
+    public function removeTokenCookie()
+    {
+        if (cookies()->has($this->sessionKey . '_token'))
+        {
+            $this->setTokenCookie('');
+        }
     }
 
 }
